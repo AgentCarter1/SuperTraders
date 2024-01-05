cf72a83fe0e5effeaf22089eea1e1886311006b2
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2024-01-05 12:26:49 +0300

    Users db service is done.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { UsersService } from './user.service';
+import { User } from './model/user.model';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('users')
+@ApiTags('User')
+@Controller('user')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('create')
+  async create(@Body() user: Partial<User>): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Get(':id')
+  async findOneById(@Param('id') id: number): Promise<User | null> {
+    return this.usersService.findOneById(id);
+  }
 
   @Get()
-  getHello() {
-    return this.userService.findAll();
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updatedUser: Partial<User>,
+  ): Promise<number> {
+    return this.usersService.update(id, updatedUser);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<number> {
+    return this.usersService.remove(id);
   }
 }
